d8f65ecd6689a9aa8b08569c53c74a06aee3f027
shui-clouds <57289318+shui-clouds@users.noreply.github.com>
2022-07-04 01:48:32 +0100

    feat: exported types for trpc input & outputs

--- pages/api/trpc/[trpc].ts
+++ pages/api/trpc/[trpc].ts
@@ -1,4 +1,4 @@
-import {inferProcedureOutput} from '@trpc/server'
+import {inferProcedureInput, inferProcedureOutput} from '@trpc/server'
 import * as trpcNext from '@trpc/server/adapters/next'
 import {appRouter, AppRouter} from '@/backend/router'
 
@@ -7,8 +7,11 @@ export default trpcNext.createNextApiHandler({
 	router: appRouter,
 	createContext: () => null,
 })
+export type TQuery = keyof AppRouter['_def']['queries']
+export type TMutation = keyof AppRouter['_def']['mutations']
 
-// eslint-disable-next-line @typescript-eslint/naming-convention
-export type inferQueryResponse<
-	TRouteKey extends keyof AppRouter['_def']['queries'],
-> = inferProcedureOutput<AppRouter['_def']['queries'][TRouteKey]>
+export type InferQueryResponse<TRouteKey extends TQuery> = inferProcedureOutput<AppRouter['_def']['queries'][TRouteKey]>
+
+export type InferMutationOutput<TRouteKey extends TMutation> = inferProcedureOutput<AppRouter['_def']['mutations'][TRouteKey]>
+
+export type InferMutationInput<TRouteKey extends TMutation> = inferProcedureInput<AppRouter['_def']['mutations'][TRouteKey]>
