a65240e81a151342ca8742507d84a43185e9b565
shui-clouds <57289318+shui-clouds@users.noreply.github.com>
2022-07-07 19:17:41 +0100

    feat: transaction table draft design

---
+++ components/tenants/TransactionForm.tsx
@@ -0,0 +1,68 @@
+import React from 'react'
+
+import {Button, TextInput, Group} from '@mantine/core'
+
+import {useForm} from '@mantine/form'
+import {trpc} from '@/utils/trpc'
+import {InferQueryResponse} from '@/pages/api/trpc/[trpc]'
+import {sendNotification} from '@/lib/component-helper'
+
+type Tenant = InferQueryResponse<'get-tenant'>
+
+export default function TenantForm({tenant, onSuccessHandler}: {tenant?: Tenant, onSuccessHandler: (error?: string) => void}) {
+	const tenantForm = useForm({
+		initialValues: {
+			name: tenant ? tenant.name : '',
+			email: tenant ? tenant.email : '',
+			balance: 0,
+			createdAt: '',
+		},
+	})
+
+	const updateTenantMutation = trpc.useMutation('update-tenant', {
+		onSuccess() {
+			onSuccessHandler()
+			sendNotification({success: true})
+		},
+		onError() {
+			sendNotification({success: false})
+		},
+	})
+
+	const createTenantMutation = trpc.useMutation('create-tenant', {
+		onSuccess(input) {
+			onSuccessHandler()
+			sendNotification({message: `${input.tenant.name} has been added.`, success: true})
+		},
+		onError(input) {
+			sendNotification({message: `${input.message}`, success: false})
+		},
+	})
+
+	return (
+		<form
+			style={{marginBottom: 10}}
+			onSubmit={tenantForm.onSubmit((values) => {
+				if (tenant) {
+					updateTenantMutation.mutate({
+						...values,
+						email: values.email ?? '',
+						id: tenant.id,
+					})
+				} else {
+					createTenantMutation.mutate({
+						...values,
+						email: values.email ?? '',
+						createdAt: new Date().toISOString(),
+					})
+				}
+			})}
+		>
+			<TextInput required style={{marginBottom: 10}} label='Full Name' placeholder='Appears on contract' {...tenantForm.getInputProps('name')} />
+			<TextInput style={{marginBottom: 10}} label='Email' placeholder='Optional' {...tenantForm.getInputProps('email')} />
+			<Group position='right' mt='md'>
+				<Button variant='light' disabled={updateTenantMutation.isLoading} type='submit'>{tenant ? 'Save' : 'Register'}</Button>
+			</Group>
+		</form>
+	)
+}
--- components/tenants/TransactionTable.tsx
+++ components/tenants/TransactionTable.tsx
@@ -1,4 +1,4 @@
-import {Table, Card, Title, Divider, Group, ActionIcon} from '@mantine/core'
+import {Table, Container, Card, Text, Badge, Title, Divider, Group, ActionIcon} from '@mantine/core'
 import React from 'react'
 import {Cash, Pencil, Plus} from 'tabler-icons-react'
 import {InferQueryResponse} from '../../pages/api/trpc/[trpc]'
@@ -9,7 +9,8 @@ type Transaction = Transactions[number]
 export default function TransactionTable({transactions}: {transactions: Transactions}) {
 	const rows = transactions.map((t: Transaction) => (
 		<tr key={t.id}>
-			<td>{t.amount}</td>
+			<td>£{t.amount}</td>
+			<td><Badge color='grape'>Rent</Badge></td>
 			<td>{t.reference}</td>
 			<td>{t.date}</td>
 			<td>
@@ -21,28 +22,33 @@ export default function TransactionTable({transactions}: {transactions: Transact
 	))
 	return (
 		<Card style={{marginBottom: 10}} withBorder radius='md' p='md'>
-			<Group position='apart'>
+			<Container>
+				<Group position='apart'>
 
-				<Group position='left' spacing='xs'>
-					<Cash color='teal' />
-					<Title order={4}>Transactions</Title>
+					<Group position='left' spacing='xs'>
+						<Cash color='teal' />
+						<Title order={4}>Transactions</Title>
+					</Group>
+					<ActionIcon variant='transparent'>
+						<Plus color='teal' size={30} />
+					</ActionIcon>
 				</Group>
-				<ActionIcon variant='transparent'>
-					<Plus color='teal' size={30} />
-				</ActionIcon>
-			</Group>
-			<Divider variant='dashed' size='sm' my='sm' />
-			<Table verticalSpacing='sm' horizontalSpacing='sm' fontSize='md'>
-				<thead>
-					<tr>
-						<th>Amount</th>
-						<th>Reference</th>
-						<th>Date</th>
-						<th> </th>
-					</tr>
-				</thead>
-				<tbody>{rows}</tbody>
-			</Table>
+				<Divider variant='dashed' size='sm' my='sm' />
+				<Table verticalSpacing='sm' horizontalSpacing='sm' fontSize='md'>
+					<thead>
+						<tr>
+							<th>Amount</th>
+							<th>Category</th>
+							<th>Reference</th>
+							<th>Date</th>
+							<th> </th>
+						</tr>
+					</thead>
+					<tbody>{rows}</tbody>
+				</Table>
+				<Divider variant='dotted' size='md' my='sm' />
+				<Title order={5} style={{marginLeft: 5, marginTop: 10}}>Total £250.30</Title>
+			</Container>
 		</Card>
 	)
 }
