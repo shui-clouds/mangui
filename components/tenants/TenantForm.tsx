2e90fbe67f58668a120737ee4a50fe4708c6184f
shui-clouds <57289318+shui-clouds@users.noreply.github.com>
2022-07-19 17:00:16 +0100

    chore: renamed tenant details component & notifications lib import

--- components/tenants/TenantForm.tsx
+++ components/tenants/TenantForm.tsx
@@ -5,7 +5,7 @@ import {Button, TextInput, Group} from '@mantine/core'
 import {useForm} from '@mantine/form'
 import {trpc} from '@/utils/trpc'
 import {InferQueryResponse} from '@/pages/api/trpc/[trpc]'
-import {sendNotification} from '@/lib/component-helper'
+import {sendNotification} from '@/lib/notifications'
 
 type Tenant = InferQueryResponse<'get-tenant'>
 
