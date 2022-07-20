777fff3cbd36d259c3a96eef84f3114879e6f335
shui-clouds <57289318+shui-clouds@users.noreply.github.com>
2022-07-20 12:46:18 +0100

    enh: tenant details card styled with tailwind + removed redundant styling

--- components/tenants/DetailsCard.tsx
+++ components/tenants/DetailsCard.tsx
@@ -18,56 +18,45 @@ export default function TenantDetailsCard({tenant}: {tenant: Exclude<Parameters<
 	}
 
 	return (
-		<Card sx={{marginBottom: 10}} withBorder radius='md' p='md'>
-			<Container>
-				<Group position='apart' sx={{marginTop: 5}}>
-					<Group position='left' spacing='xs'>
-						<Title order={4}>{tenant.name}</Title>
-						<User size={20} />
-					</Group>
-
-					<Group position='right'>
-						<ActionIcon color='orange' variant='transparent' onClick={openTenantEditModal}>
-							<Edit color='orange' size={30} />
-						</ActionIcon>
-					</Group>
+		<Card className='mb-5 p-2' withBorder>
+			<Group className=' ml-4 mt-2'>
+				<Title className='text-lg'>{tenant.name}</Title>
+				<ActionIcon variant='transparent' onClick={openTenantEditModal}>
+					<Edit color='orange' />
+				</ActionIcon>
+			</Group>
+			<Container className='space-y-3'>
+				<Text color='dimmed'>{tenant.email}</Text>
+				<Divider className='my-3' variant='dashed' size='sm' />
+				<Group spacing='xs'>
+					<Home color='purple' />
+					<Text className='font-semibold' color='purple'>Penhurst</Text>
+					<Badge className='rounded-md' size='md' variant='outline'>
+						PRESENT
+					</Badge>
+				</Group>
+				<Group spacing='xs'>
+					<BuildingBank color='teal' />
+					<Text weight={600} color='teal'>£{tenant.balance}</Text>
+				</Group>
+				<Group spacing='xs'>
+					<DoorEnter color='orange' />
+					<Text className='font-medium' color='gray'>2nd April 2021</Text>
+				</Group>
+				<Group spacing='xs'>
+					<DoorExit color='orange' />
+					<Text className='font-medium' color='gray'>12th November 2022</Text>
+					<Text className='font-medium' color='gray'>1 month, 2 days remaining</Text>
 				</Group>
 
-				<Text sx={{marginBottom: 10}} color='dimmed'>{tenant.email}</Text>
-				<Divider variant='dashed' size='sm' my='sm' />
-
-				<Stack my='sm' align='flex-start' justify='space-around'>
-					<Group position='left' spacing='xs'>
-						<Home color='purple' />
-						<Text weight={700} color='purple'>Penhurst</Text>
-						<Badge size='md' radius='sm' color='teal' variant='outline'>
-							PRESENT
-						</Badge>
-					</Group>
-
-					<Group position='left' spacing='xs'>
-						<BuildingBank color='teal' />
-						<Text weight={600} color='teal'>£{tenant.balance}</Text>
-					</Group>
-					<Group position='left' spacing='xs'>
-						<DoorEnter color='orange' />
-						<Text weight={500} color='gray'>2nd April 2021</Text>
-					</Group>
-					<Group position='left' spacing='xs'>
-						<DoorExit color='orange' />
-						<Text weight={500} color='gray'>12th November 2022</Text>
-						<Text weight={400} color='gray'>1 month, 2 days remaining</Text>
-					</Group>
-				</Stack>
-
-				<Group sx={{marginTop: 20}} position='left' spacing='sm'>
-					<Button size='sm' radius='md' variant='light' color='orange' leftIcon={<Pencil size={20} />}>
+				<div className='flex justify-start space-x-3 py-2'>
+					<Button className='rounded-lg' size='sm' variant='light' color='orange' leftIcon={<Pencil />}>
 						Update Tenancy
 					</Button>
-					<Button size='sm' radius='md' variant='light' color='teal' leftIcon={<Plus size={20} />}>
+					<Button className='rounded-lg' size='sm' variant='light' color='teal' leftIcon={<Plus />}>
 						Balance
 					</Button>
-				</Group>
+				</div>
 			</Container>
 		</Card>
 	)
