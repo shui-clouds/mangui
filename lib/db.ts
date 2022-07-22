b06c4aba8bd4bb7a61a0789f44fd42afe6f94cf5
shui-clouds <57289318+shui-clouds@users.noreply.github.com>
2022-07-22 20:36:54 +0100

    feat: db lib converts date props into strings for ssr

--- lib/db.ts
+++ lib/db.ts
@@ -1,11 +1,17 @@
 import {prisma} from '@/backend/utils/prisma'
 
-export const getTenants = async () => prisma.tenant.findMany()
+export async function getTenants() {
+	const rawTenants = await prisma.tenant.findMany()
+	const tenants = rawTenants.map((t) => ({...t, createdAt: ''}))
+
+	return tenants
+}
 
 export async function getTransactions(tenantId: string) {
-	const transactions = prisma.transaction.findMany({
+	const rawTransactions = await prisma.transaction.findMany({
 		where: {tenantId},
 	})
+	const transactions = rawTransactions.map((t) => ({...t, date: ''}))
 	return transactions
 }
 
@@ -18,10 +24,14 @@ export async function getTransaction(transactionId: string) {
 }
 
 export async function getTenant(tenantId: string) {
-	const tenant = await prisma.tenant.findUnique({
+	const rawTenant = await prisma.tenant.findUnique({
 		where: {id: tenantId},
 		include: {transactions: true},
 	})
-	if (!tenant) throw Error(`No tenant found with id ${tenantId}`)
+	if (!rawTenant) throw Error(`No tenant found with id ${tenantId}`)
+
+	const transactions = rawTenant.transactions.map((t) => ({...t, date: ''}))
+	const tenant = {...rawTenant, createdAt: '', transactions}
+
 	return tenant
 }
