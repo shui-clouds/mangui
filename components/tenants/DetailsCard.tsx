b7b3e553e082c3189f02e1b70829bd5aa56b35e1
shui-clouds <57289318+shui-clouds@users.noreply.github.com>
2022-09-04 21:00:26 +0100

    enh: cleaned up tenant details card

--- components/tenants/DetailsCard.tsx
+++ components/tenants/DetailsCard.tsx
@@ -1,6 +1,6 @@
 import {Group, Text, Divider, Container} from '@mantine/core'
 import React from 'react'
-import {BuildingBank, DoorExit, DoorEnter, Home} from 'tabler-icons-react'
+import {DoorExit, DoorEnter, Home} from 'tabler-icons-react'
 import TenantForm from './TenantForm'
 
 export default function TenantDetailsCard({tenant}: {tenant: Exclude<Parameters<typeof TenantForm>[0]['tenant'], undefined>}) {
@@ -12,10 +12,6 @@ export default function TenantDetailsCard({tenant}: {tenant: Exclude<Parameters<
 			<Group className='my-3' spacing='xs'>
 				<Home color='purple' />
 				<Text className='font-semibold' color='purple'>Penhurst</Text>
-			</Group>
-			<Group className='my-3' spacing='xs'>
-				<BuildingBank color='teal' />
-				<Text className='font-medium' color='teal'>Account Balance: £{tenant.balance}</Text>
 			</Group>
 			<Group className='my-3' spacing='xs'>
 				<DoorEnter color='orange' />
