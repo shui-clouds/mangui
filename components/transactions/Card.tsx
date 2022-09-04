930576cf47ddd660b44708df12251764517d95c0
shui-clouds <57289318+shui-clouds@users.noreply.github.com>
2022-09-04 20:59:19 +0100

    feat: added transaction card

---
+++ components/transactions/Card.tsx
@@ -0,0 +1,35 @@
+import {Text, Card, Image, Group, Badge, Button} from '@mantine/core'
+import React from 'react'
+import {InferQueryResponse} from '../../pages/api/trpc/[trpc]'
+
+type Transactions = InferQueryResponse<'get-transactions'>
+
+export default function TransactionCard({transaction}: {transaction: Transactions[number]}) {
+	return (
+		<Card className='my-2' p='lg' radius='md' withBorder>
+			<Group position='apart' mt='xs' mb='xs'>
+				<Text weight={500}>{transaction.date}</Text>
+				<Badge color='gray' variant='light'>
+					{transaction.id}
+				</Badge>
+			</Group>
+
+			<Text size='md' color='black'>
+				Amount
+			</Text>
+			<Text size='lg' className={`${transaction.amount > 0 ? 'text-emerald-500' : 'text-red-500'}`}>
+				{transaction.amount}
+			</Text>
+
+			<Text size='md' color='black'>
+				Reference
+			</Text>
+			<Text size='lg'>
+				{transaction.reference}
+			</Text>
+			{/* <Button variant='light' color='blue' fullWidth mt='md' radius='md'>
+				WIN
+			</Button> */}
+		</Card>
+	)
+}
