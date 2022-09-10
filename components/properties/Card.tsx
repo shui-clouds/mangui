e2ca9ad307f400ea92f72c1cc6a58a857c72251d
shui-clouds <57289318+shui-clouds@users.noreply.github.com>
2022-09-10 12:32:17 +0100

    feat: property card

--- components/properties/Card.tsx
+++ components/properties/Card.tsx
@@ -1,11 +1,15 @@
-import {Group, Text, Divider, Container} from '@mantine/core'
+import {Group, Text, Divider, Card} from '@mantine/core'
+import Link from 'next/link'
 import React from 'react'
-import {Pig, DoorEnter, Home} from 'tabler-icons-react'
+import {Pig, Home} from 'tabler-icons-react'
 import PropertyForm from './Form'
 
 export default function PropertyCard({property}: {property: Exclude<Parameters<typeof PropertyForm>[0]['property'], undefined>}) {
 	return (
-		<>
+		<Card className='my-4' withBorder>
+			<h1 className='font-medium text-xl'>
+				<Link href={`/properties/${property.id}`}>{property.name}</Link>
+			</h1>
 			<Text className='my-2' color='dimmed'>{property.address}</Text>
 			<Divider className='mb-3 mt-2' variant='dashed' size='sm' />
 			<Group spacing='xs'>
@@ -16,6 +20,6 @@ export default function PropertyCard({property}: {property: Exclude<Parameters<t
 				<Pig className='text-green-600' />
 				<Text className='font-medium text-green-600'>£2500 / month</Text>
 			</Group>
-		</>
+		</Card>
 	)
 }
