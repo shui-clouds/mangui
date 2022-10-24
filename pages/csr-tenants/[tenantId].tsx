c95198b01e076ce8bf14775edf362fe69ef3bc98
shui-clouds <57289318+shui-clouds@users.noreply.github.com>
2022-10-25 00:55:55 +0100

    enh: updated tenant page for csr

--- pages/csr-tenants/[tenantId].tsx
+++ pages/csr-tenants/[tenantId].tsx
@@ -55,26 +55,25 @@ export default function TenantPage() {
 	tenant.transactions.sort((a, b) => new Date(b.date) - new Date(a.date))
 
 	return (
-		<Container size='md'>
-			<Title className='my-5 text-2xl'>{tenant.name}</Title>
-			<Link href='/csr-tenants'>Back</Link>
-			<Title className='my-2 font-medium text-xl'>Overview</Title>
-			<Card className='mb-5 p-2' withBorder>
-				<TenantDetailsCard tenant={tenant} />
-				<div className='flex justify-start space-x-3 py-2'>
-					<Button className='ml-4 rounded-lg' size='sm' variant='light' color='orange' onClick={openTenantEditModal}>
-						Edit Details
-					</Button>
-				</div>
-			</Card>
-			<Group position='apart' className='my-5'>
-				<Text className='my-2 text-xl font-medium'>Account Balance: £{balance}</Text>
+		<Container size='md' className='mb-10'>
+			<Group position='apart' spacing='sm'>
+				<Title className='my-5 text-2xl'>Overview</Title>
+				<Button className='rounded-lg' size='sm' variant='light' onClick={openTenantEditModal}>
+					Edit Details
+				</Button>
+			</Group>
+			<TenantDetailsCard key={tenant.id} tenant={tenant} />
+			<Group position='apart' spacing='sm'>
+				<Text className='my-4 text-xl font-medium'>Account Balance: £{balance}</Text>
 				<Button className='rounded-md' size='sm' variant='light' onClick={openTransactionCreateModal}>
 					Add Transaction
 				</Button>
 			</Group>
 			<Title className='my-2 font-medium text-xl'>Transaction History</Title>
 			{tenant.transactions.map((t) => (<TransactionCard transaction={t} />))}
-		</Container>
+			<div className='my-8'>
+				<Link href='/csr-tenants'>Back</Link>
+			</div>
+		</Container >
 	)
 }
--- pages/csr-tenants/index.tsx
+++ pages/csr-tenants/index.tsx
@@ -1,15 +1,16 @@
 import type React from 'react'
-import {useModals} from '@mantine/modals'
-import {Button, Card, Title} from '@mantine/core'
+import { useModals } from '@mantine/modals'
+import { Button, Card, Container, Title } from '@mantine/core'
 import Link from 'next/link'
-import {trpc} from '@/utils/trpc'
+import { trpc } from '@/utils/trpc'
 import TenantDetailsCard from '@/components/tenants/DetailsCard'
 import TenantForm from '@/components/tenants/TenantForm'
+import tenants from '@/remix/app/routes/tenants'
 
 export default function TenantsPage() {
 	const modals = useModals()
 
-	const {data: currentTenants, isLoading} = trpc.useQuery(
+	const { data: currentTenants, isLoading } = trpc.useQuery(
 		['get-tenants'],
 		{
 			refetchInterval: false,
@@ -29,17 +30,17 @@ export default function TenantsPage() {
 	if (isLoading) return 'LODING WAAAA'
 
 	return (
-		<>
+		<Container size='md' className='mb-10'>
 			<Title className='my-4 font-medium text-2xl'>Tenants</Title>
 			<Button variant='light' radius='md' size='sm' onClick={openContentModal}>New Tenant</Button>
 			{currentTenants?.map((tenant) => (
-				<Card withBorder className='my-4'>
-					<h1 className='ml-4 font-medium text-xl'>
-						<Link href={`csr-tenants/${tenant.id}`}>{tenant.name}</Link>
-					</h1>
+				<Card key={tenant.id} withBorder className='my-4'>
 					<TenantDetailsCard key={tenant.id} tenant={tenant} />
+					<h1 className='font-medium text-lg'>
+						<Link href={`/csr-tenants/${tenant.id}`}>View</Link>
+					</h1>
 				</Card>
 			))}
-		</>
+		</Container>
 	)
 }
